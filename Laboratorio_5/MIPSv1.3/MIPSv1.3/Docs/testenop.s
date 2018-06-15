015ce824654ac3a18f08e0ee28224640ae8763b5
Henrique Mendes de Freitas Mariano <henriquiui70@gmail.com>
2018-06-15 15:19:44 -0300

    Tentando o Lab5

---
+++ Laboratorio_5/MIPSv1.3/MIPSv1.3/Docs/testenop.s
@@ -0,0 +1,12 @@
+.data
+
+.text
+addi $t0, $t0, 1
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+addi $t1, $t0, 1
--- Laboratorio_5/MIPSv1.3/MIPSv1.3/Docs/testes/testeSIMPLESPIPE.s
+++ Laboratorio_5/MIPSv1.3/MIPSv1.3/Docs/testes/testeSIMPLESPIPE.s
@@ -1,22 +1,20 @@
 # Teste para verificacao da simulacao por forma de onda e na DE1-SoC
 .data
-	WORD: .word 0xB0DEB0B0, 0xF0CAF0FA, 0x00400070
+	WORD: .word 0xB0DEB0B0, 0xF0CAF0FA, 0x00400068
 .text
 	lui $t0,0x1001
-	nop
-	nop
 	ori $t0,$t0,0x0000
-	nop
-	nop
 	add $t1,$zero,$t0
-	nop
-	nop
 	lw $t0,0($t1)
+	nop
+	nop
 	addi $t0,$t0,1
 	beq $t0,$zero,PULA
 	addi $t0,$t0,1
 	addi $t0,$t0,1
 PULA:	lw $t2,0($t1)
+	nop
+	nop
 	bne $t0,$t2,PULA2
 	addi $t0,$t1,-1
 	addi $t0,$t0,-1
